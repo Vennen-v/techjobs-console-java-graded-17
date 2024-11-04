fa957d7ff4eea6a8310670effe7e098e8948d124
Vennen-v <chgolliday@gmail.com>
2024-11-04 16:13:11 -0600

    Finished I hope

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -56,12 +56,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -70,12 +70,12 @@ public class JobData {
         loadData();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
-
+        String caseInsensitive = value.toUpperCase();
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toUpperCase().contains(caseInsensitive)) {
                 jobs.add(row);
             }
         }
@@ -87,7 +87,7 @@ public class JobData {
      * Search all columns for the given term
      *
      * @param value The search term to look for
-     * @return      List of all jobs with at least one field containing the value
+     * @return List of all jobs with at least one field containing the value
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
 
@@ -95,7 +95,18 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        String caseInsensitive = value.toUpperCase();
+        for (HashMap<String, String> row: allJobs) {
+            for (String val: row.values()) {
+                if (val.toUpperCase().contains(caseInsensitive)) {
+                    jobs.add(row);
+                    break;
+                }
+            }
+        }
+        return jobs;
     }
 
     /**
@@ -138,5 +149,6 @@ public class JobData {
             e.printStackTrace();
         }
     }
+
 
 }
